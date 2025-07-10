525fbc18098cf161361ac54d2213f925d3410b89
sandeep821110 <sandeepku821110@gmail.com>
2025-07-10 12:49:49 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <div className="App flex flex-col items-center justify-center text-white bg-gray-100 min-h-screen bg-violet-900">
       <div className='flex flex-col items-center justify-center bg-gray-900 bg-opacity-50 rounded-lg shadow-lg p-6 m-4 w-full max-w-md'>
         <h1 className='text-shadow-gray-300  text-white font-semibold p-4 justify-items-center items-center text-2xl m-2 md:text-center sm:text-center sm:text-xl md:text-xl lg:w-full lg:text-xl '>
-        Weather App
+        Weather App Think
       </h1>
       <Weather />
       </div>
